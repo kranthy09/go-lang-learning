5087b63b95cb34427d5889fdba91b66e53d82ee7
skywalker <g.kranthi2507@gmail.com>
2021-05-13 20:06:45 +0530

    prints rem for numbers between 10 and 100 when divided by 4

--- Practices/control-flow/using_operators_in_for_loop.go
+++ Practices/control-flow/using_operators_in_for_loop.go
@@ -9,6 +9,8 @@ func main() {
 	// declare variables and assign 10, 100 values using short declaration
 	a := 10
 	b := 100
+
+	// using for loop, prints rem for numbers between 10, 100
 	for i := a; i <= b; i++ {
 		fmt.Printf("Remainder: %v, when divided %v by %v\n", i%4, i, 4)
 	}
