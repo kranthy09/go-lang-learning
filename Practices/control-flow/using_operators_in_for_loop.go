8a91d4dfdc19701b4026cabaaac375577b385ebc
skywalker <g.kranthi2507@gmail.com>
2021-05-13 20:05:02 +0530

    prints remainder when number divided by 4

---
+++ Practices/control-flow/using_operators_in_for_loop.go
@@ -0,0 +1,15 @@
+// Print out the remainder (modulus)
+// which is found for each number between 10 and 100 when it is divided by 4.
+
+package main
+
+import "fmt"
+
+func main() {
+	// declare variables and assign 10, 100 values using short declaration
+	a := 10
+	b := 100
+	for i := a; i <= b; i++ {
+		fmt.Printf("Remainder: %v, when divided %v by %v\n", i%4, i, 4)
+	}
+}
