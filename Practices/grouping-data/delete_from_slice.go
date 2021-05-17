da7f3c8657025ad70b01f90baa0832d914d172bd
skywalker <g.kranthi2507@gmail.com>
2021-05-17 17:00:46 +0530

    drop/delete values from a slice

--- Practices/grouping-data/delete_from_slice.go
+++ Practices/grouping-data/delete_from_slice.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 4, 3, 67, 109}
 
 	// delete from the slice
 	x = x[2:] // drop first two values from the slice
+	fmt.Println(x)
+
+	x = append(x[:3], x[6:]...) // drop values from index 3 to 6
 	fmt.Println(x)
 
 	y := x[:5]
