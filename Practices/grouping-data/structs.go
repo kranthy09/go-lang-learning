572e58c7ab6cb0fcf1d42c5c192deeedde91cea9
skywalker <g.kranthi2507@gmail.com>
2021-05-18 16:23:25 +0530

    create struct by type and anonymous struct and print it

--- Practices/grouping-data/structs.go
+++ Practices/grouping-data/structs.go
@@ -9,12 +9,14 @@ type person struct {
 }
 
 func main() {
+	// create a struct of TYPE person
 	p1 := person{
 		first:                      "kranthi",
 		last:                       "kumar",
 		favorite_ice_cream_flvaour: []string{"chocolate", "vanilla", "butterscotch"},
 	}
 
+	// create a struct of TYPE person
 	p2 := person{
 		first:                      "nobita",
 		last:                       "doremon",
@@ -33,5 +35,17 @@ func main() {
 	for index, value := range p2.favorite_ice_cream_flvaour {
 		fmt.Println(index, value)
 	}
+
+	// creating struct of anonymous type
+	anonymous_struct := struct {
+		name string
+		age  int
+	}{
+		name: "skywalker",
+		age:  25,
+	}
+
+	fmt.Println("print anonymous struct")
+	fmt.Println(anonymous_struct)
 
 }
