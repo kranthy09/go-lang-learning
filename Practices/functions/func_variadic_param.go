4722f2502cf3b63e517be0161abe87bf5b27e3ac
skywalker <g.kranthi2507@gmail.com>
2021-05-21 11:22:52 +0530

    function with variadic parameter

---
+++ Practices/functions/func_variadic_param.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+// create a function which taked variadic parameter
+func sum_of_numbers(x ...int) int {
+	total := 0
+	for _, v := range x {
+		total += v
+	}
+	return total
+}
+
+func main() {
+	x := []int{1, 2, 3, 4, 5, 6}
+
+	// passing slice x value to func by unfrilling it with "..."
+	result := sum_of_numbers(x...)
+	fmt.Println(result) // prints the result that returned when the func is called
+}
